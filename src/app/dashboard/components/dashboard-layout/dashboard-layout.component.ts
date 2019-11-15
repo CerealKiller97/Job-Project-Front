c445d3b04a8601d29e6c11b656c772192ee6de79
CerealKiller97 <bogdanovic.stefan@outlook.com>
2019-11-15 08:47:25 +0100

    import clean up

--- src/app/dashboard/components/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/components/dashboard-layout/dashboard-layout.component.ts
@@ -1,9 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from "../../../shared/services/auth.service";
-import {HttpErrorResponse} from "@angular/common/http";
-import {Subscription} from "rxjs";
 import {Router} from "@angular/router";
-import * as M from  "materialize-css/dist/js/materialize";
+
 @Component({
   selector: 'app-dashboard-layout',
   templateUrl: './dashboard-layout.component.html',
